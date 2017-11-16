842400281b700ae81e4140d39d2003d26f02e0cb
Jimmy Beckett <jimmybeckett1@gmail.com>
2017-11-16 09:48:20 -0600

    Added header files for trig, exponential, and other functions

---
+++ other_functions.h
@@ -0,0 +1,15 @@
+namespace acp {
+	double abs(const complex);
+
+	double arg(const complex);
+
+	complex conj(const complex);
+
+	double imag(const complex&);
+
+	double norm(const complex);
+
+	complex polar(double mag, double ang=0.0);
+
+	double real(const complex&);
+}
