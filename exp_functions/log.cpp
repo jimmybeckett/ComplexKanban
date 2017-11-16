b983263f9818aea7a93c0898f92f557d2e717306
Jimmy Beckett <jimmybeckett1@gmail.com>
2017-11-16 12:36:47 -0600

    Added directories for functions

---
+++ exp_functions/log.cpp
@@ -0,0 +1,10 @@
+#include <../exp_functions.h>
+#include <math.h>
+
+namespace acp {
+        complex log(const complex c) {
+                //not implemented yet
+
+                return c;
+        }
+}
---
+++ other_functions/abs.cpp
@@ -0,0 +1,9 @@
+#include <../other_functions.h>
+#include <math.h>
+
+namespace acp {
+        complex abs(const complex c) {
+                //not implemented yet
+                return c;
+        }
+}
