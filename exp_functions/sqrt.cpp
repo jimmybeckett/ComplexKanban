380c99bc2d0767e0acea164a7009a8b293d21009
Jimmy Beckett <jimmybeckett1@gmail.com>
2017-12-09 23:06:17 -0600

    Fixed sqrt

--- exp_functions/sqrt.cpp
+++ exp_functions/sqrt.cpp
@@ -1,13 +1,8 @@
-#include "../other_functions.h"
-#include "../polar_struct.h"
-#include <cmath>
+#include "../exp_functions.h"
+#include <math.h>
 
 namespace acp {
-	double sqrt(const complex c) {
-		polar_t p = polar(c);
-		double ncos = cos(acos(p.costheta)/2);
-		double nsin = sin(asin(p.sintheta)/2);
-		double nr = sqrt(p.r);
-		return abs(complex(nr, 0) * (complex(ncos, 0) + complex(0, 1) * complex(nsin, 0)));
+	complex sqrt(const complex c) {
+		return pow(c, .5);
     }
 }
--- unit_tests/tests/sqrt_test.cpp
+++ unit_tests/tests/sqrt_test.cpp
@@ -4,17 +4,9 @@
 namespace acp {
 	void sqrt_test() {
 		std::cout << "Running sqrt tests..." << std::endl;
-		std::cout << "A";
-		std::cout << sqrt(complex(4, 4)) << std::endl;
 		assert(in_range(sqrt(complex(4, 4)), complex(2.1973682, 0.9101797), complex_error));
-		std::cout << "B";
-		std::cout << sqrt(complex(7, -2)) << std::endl;
 		assert(in_range(sqrt(complex(7, -2)), complex(2.6720881, -0.3742391), complex_error));
-		std::cout << "C";
-		std::cout << sqrt(complex(-4.332, -9.87)) << std::endl;
 		assert(in_range(sqrt(complex(-4.332, -9.87)), complex(1.7953868, -2.7487112), complex_error));
-		std::cout << "D";
-		std::cout << sqrt(complex(0, 0)) << std::endl;
 		assert(abs(sqrt(complex(0, 0)) - complex(0, 0)) == 0);
 		std::cout << "Sqrt tests passed!" << std::endl;
 	}
