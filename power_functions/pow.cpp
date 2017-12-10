5a43fcf4b2023258cbb5c779b32bfc00e227bb12
Jimmy Beckett <jimmybeckett1@gmail.com>
2017-12-09 23:04:24 -0600

    Overloaded pow

--- power_functions/pow.cpp
+++ power_functions/pow.cpp
@@ -7,33 +7,24 @@ namespace acp {
 	complex pow(const complex b, const complex exp) {
 		complex arg = exp * log(b);
 		return complex(std::pow(M_E, arg.get_re()) * std::cos(arg.get_im()), std::pow(M_E, arg.get_re()) * std::sin(arg.get_im()));
-		//double arg_1 = std::pow(b.get_re() * b.get_re() + b.get_im() * b.get_im(), exp.get_re() / 2);
-		//double arg_2 = std::exp(-exp.get_im() * arg(b));
-		//double arg_3 = exp.get_re() * arg(b);
-		//double arg_4 = exp.get_im() * std::log(b.get_re() * b.get_re() + b.get_im() * b.get_im()) / 2;
-		//temp.set_re(arg_1  * arg_2 * std::cos(arg_3 + arg_4));
-		//temp.set_im(std::sin(exp.get_re() * arg(b) + arg_4));
-		//return temp;
 	}
 }
 
 namespace acp {
 	complex pow(const double b, const complex exp) {
-
-		return complex(0, 0);
+		return complex(std::pow(b, exp.get_re()) * std::cos(exp.get_im() * std::log(b)), std::pow(b, exp.get_re()) * std::sin(exp.get_im() * std::log(b)));
 	}
 }
 
 namespace acp {
 	complex pow(const complex b, const double exp) {
-
-		return complex(0, 0);
+		complex temp(exp * log(b).get_re(), exp * log(b).get_im());
+		return pow(M_E, temp);
 	}
 }
 
 namespace acp {
 	complex pow(const complex b, const int exp) {
-
-		return complex(0, 0);
+		return pow(b, (double)exp);
 	}
 }
--- unit_tests/tests/pow_test.cpp
+++ unit_tests/tests/pow_test.cpp
@@ -5,10 +5,17 @@
 namespace acp {
 	void pow_test() {
 		std::cout << "Running pow tests..." << std::endl;
+		//complex ^ complex
 		assert(in_range(pow(complex(2, 3), complex(4, 5)), complex(-0.7530458, -0.9864287), complex_error));
 		assert(in_range(pow(complex(6.825, 5.654), complex(1.234, 2.876)), complex(1.3393532, 1.5110442), complex_error));
 		assert(in_range(pow(complex(7, 1.8), complex(-2.8, -2.223)), complex(0.0026122, 0.0063681), complex_error));
 		assert(in_range(pow(complex(-3.52, -1.54), complex(-3, 2.987)), complex(57.3053654, -21.4546935), complex_error));
+		//complex ^ double
+		assert(in_range(pow(complex(-3.524, 3.76), -2.745), complex(0.0110494, -0.0010602), complex_error));
+		//complex ^ int
+		assert(in_range(pow(complex(3, 4), 4), complex(-527, -336), complex_error));
+		//double ^ complex
+		assert(in_range(pow(2.345, complex(-.02, 3)), complex(-0.8197643, 0.5426504), complex_error));
 		std::cout << "Pow tests passed!" << std::endl;
 	}
 }
