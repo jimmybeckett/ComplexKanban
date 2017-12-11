c6ec5e11398bae71d08943ad06af4eac69ee2650
Russell Coleman <russellsayshi@gmail.com>
2017-12-10 20:28:51 -0600

    modified build.sh for jimmy

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo Building program... Copyright HP 15C 2017
-g++ -Wall -Werror *.cpp trig_functions/*.cpp exp_functions/*.cpp util_functions/*.cpp unit_tests/*.cpp complex_operators/*.cpp -o prog.out
+g++ -std=c++11 -Wall -Werror *.cpp trig_functions/*.cpp exp_functions/*.cpp util_functions/*.cpp unit_tests/*.cpp complex_operators/*.cpp -o prog.out
