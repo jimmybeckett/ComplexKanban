ccd03513f1b683173970d1525b550841d4598ba8
Russell Coleman <russellsayshi@gmail.com>
2017-12-10 21:08:39 -0600

    removed stuff windows didnt like for whatever reason

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo Building program... Copyright HP 15C 2017
-g++ -std=c++11 -Wall -Werror *.cpp trig_functions/*.cpp exp_functions/*.cpp util_functions/*.cpp unit_tests/*.cpp complex_operators/*.cpp -lm -o prog.out
+g++ -Wall -Werror *.cpp trig_functions/*.cpp exp_functions/*.cpp util_functions/*.cpp unit_tests/*.cpp complex_operators/*.cpp -lm -o prog.out
